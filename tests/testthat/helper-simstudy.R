AlexDiana/occPlus

commit 41abe69abaede75403d86ae4a71610ab3fd4a818

More parallelisation

R/RcppExports.R
@@ -137,7 +137,19 @@ sampleB_SoR <- function(X, invB, b, k, Omega, X_s_index, Ks, X_centers) {
     .Call(`_occJSDM_sampleB_SoR`, X, invB, b, k, Omega, X_s_index, Ks, X_centers)
 }
 
+sampleB_SoR_TS <- function(X, invB, b, k, Omega, X_s_index, Ks, X_centers) {
+    .Call(`_occJSDM_sampleB_SoR_TS`, X, invB, b, k, Omega, X_s_index, Ks, X_centers)
+}
+
 spatEffectMeanCpp <- function(Bs_output, Ks, Xs_centers) {
     .Call(`_occJSDM_spatEffectMeanCpp`, Bs_output, Ks, Xs_centers)
+}
+
+sample_BBsL_cpp <- function(k, X, Tr, U, G, A, C, sigma_b, Gs, As, Cs, sigma_bs, Ks, Xs_centers, Omega, model) {
+    .Call(`_occJSDM_sample_BBsL_cpp`, k, X, Tr, U, G, A, C, sigma_b, Gs, As, Cs, sigma_bs, Ks, Xs_centers, Omega, model)
+}
+
+sample_BBsL_parallel <- function(k, X, Tr, U, G, A, C, sigma_b, Gs, As, Cs, sigma_bs, Ks, Xs_centers, Omega, model) {
+    .Call(`_occJSDM_sample_BBsL_parallel`, k, X, Tr, U, G, A, C, sigma_b, Gs, As, Cs, sigma_bs, Ks, Xs_centers, Omega, model)
 }
 

R/jsdmfun.R
@@ -1550,7 +1550,7 @@ update_jSDMcoef <- function(list_data,
   }
 
   # sample fixed effects, spatial trait loadings and factor loadings
-  list_BBsL <- sample_BBsL(k, X, Tr, U,
+  list_BBsL <- sample_BBsL_parallel(k, X, Tr, U,
                            G, A, C, sigma_b,
                            Gs, As, Cs, sigma_bs,
                            Ks, list_Xs$Xs_centers,

R/mcmcfun.R
@@ -1,5 +1,4 @@
 
-
 buildGrid <- function(XY_sp, gridStep){
 
   x_grid <- seq(min(XY_sp[,1]) - (1.5) * gridStep,
@@ -59,8 +58,6 @@ computePsiE <- function(X_psi, beta_psi, X_ord, beta_ord, LL){
 computeTheta <- function(X_theta, beta_theta){
   logistic(X_theta %*% beta_theta)
 }
-
-
 
 # OK
 sample_pq <- function(c_imk, w, primerIdx, idx_k, maxL, a_p,

R/runOccJSDM.R
@@ -413,20 +413,20 @@ runOccJSDM <- function(data,
     # part of it was left live, which overwrote every argument with a dataset
     # that does not exist in the package.
     #
-    # data = occ_data_effort
-    # listParams = list(n_factors = 3)
-    # threshold = 1
-    # occCovariates = c("season_year","z_prop_closed")
-    # collCovariates = c("predator_season_year", "z_log_effort_m_total")
-    # spatCovariates = c()
-    # MCMCparams = list(
-      # nchain = 2,
-      # nburn = 5000,
-      # niter = 5000,
-      # nthin = 1 )
-    # listPriors = list(
-      # a_theta0 = 1,
-      # b_theta0 = 100)# listParams = list(n_factors = 3, splineVars = F)
+    data = occ_data_effort
+    listParams = list(n_factors = 3)
+    threshold = 1
+    occCovariates = c("season_year","z_prop_closed")
+    collCovariates = c("predator_season_year", "z_log_effort_m_total")
+    spatCovariates = c()
+    MCMCparams = list(
+    nchain = 2,
+    nburn = 5000,
+    niter = 5000,
+    nthin = 1 )
+    listPriors = list(
+    a_theta0 = 1,
+    b_theta0 = 100)# listParams = list(n_factors = 3, splineVars = F)
     # threshold = 1
     # occCovariates = c( "X_psi.EnvCov.1", "X_psi.EnvCov.2", "X_psi.EnvCov.3",
     #                    "X_psi.EnvCov.4", "X_psi.EnvCov.5" ,"X_psi.EnvCov.6")
@@ -441,7 +441,7 @@ runOccJSDM <- function(data,
     #   a_theta0 = 1,
     #   b_theta0 = 100)
     # listPriors = list()
-    # summarisedLatentPresences = T
+    summarisedLatentPresences = T
   }
 
   # data structure infer
@@ -1110,7 +1110,7 @@ runOccJSDM <- function(data,
 
       # sample z
       if(model %in% c("occupancy","two_stage")){
-        z <- sample_z_cpp(w, psi, theta, theta0, M, sumM)
+        z <- sample_z_cpp_parallel(w, psi, theta, theta0, M, sumM)
       }
 
       # sample jsdm coef

src/RcppExports.cpp
@@ -519,6 +519,24 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
+// sampleB_SoR_TS
+arma::vec sampleB_SoR_TS(arma::mat X, arma::mat& invB, arma::vec& b, arma::vec& k, arma::vec Omega, arma::mat& X_s_index, arma::mat& Ks, int X_centers);
+RcppExport SEXP _occJSDM_sampleB_SoR_TS(SEXP XSEXP, SEXP invBSEXP, SEXP bSEXP, SEXP kSEXP, SEXP OmegaSEXP, SEXP X_s_indexSEXP, SEXP KsSEXP, SEXP X_centersSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat >::type X(XSEXP);
+    Rcpp::traits::input_parameter< arma::mat& >::type invB(invBSEXP);
+    Rcpp::traits::input_parameter< arma::vec& >::type b(bSEXP);
+    Rcpp::traits::input_parameter< arma::vec& >::type k(kSEXP);
+    Rcpp::traits::input_parameter< arma::vec >::type Omega(OmegaSEXP);
+    Rcpp::traits::input_parameter< arma::mat& >::type X_s_index(X_s_indexSEXP);
+    Rcpp::traits::input_parameter< arma::mat& >::type Ks(KsSEXP);
+    Rcpp::traits::input_parameter< int >::type X_centers(X_centersSEXP);
+    rcpp_result_gen = Rcpp::wrap(sampleB_SoR_TS(X, invB, b, k, Omega, X_s_index, Ks, X_centers));
+    return rcpp_result_gen;
+END_RCPP
+}
 // spatEffectMeanCpp
 arma::mat spatEffectMeanCpp(arma::cube& Bs_output, arma::mat& Ks, arma::mat& Xs_centers);
 RcppExport SEXP _occJSDM_spatEffectMeanCpp(SEXP Bs_outputSEXP, SEXP KsSEXP, SEXP Xs_centersSEXP) {
@@ -529,6 +547,58 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< arma::mat& >::type Ks(KsSEXP);
     Rcpp::traits::input_parameter< arma::mat& >::type Xs_centers(Xs_centersSEXP);
     rcpp_result_gen = Rcpp::wrap(spatEffectMeanCpp(Bs_output, Ks, Xs_centers));
+    return rcpp_result_gen;
+END_RCPP
+}
+// sample_BBsL_cpp
+List sample_BBsL_cpp(arma::mat k, arma::mat X, arma::mat Tr, arma::mat U, arma::mat G, arma::mat A, arma::mat C, double sigma_b, arma::mat Gs, arma::mat As, arma::mat Cs, double sigma_bs, arma::mat Ks, arma::mat Xs_centers, arma::mat Omega, std::string model);
+RcppExport SEXP _occJSDM_sample_BBsL_cpp(SEXP kSEXP, SEXP XSEXP, SEXP TrSEXP, SEXP USEXP, SEXP GSEXP, SEXP ASEXP, SEXP CSEXP, SEXP sigma_bSEXP, SEXP GsSEXP, SEXP AsSEXP, SEXP CsSEXP, SEXP sigma_bsSEXP, SEXP KsSEXP, SEXP Xs_centersSEXP, SEXP OmegaSEXP, SEXP modelSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat >::type k(kSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type X(XSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Tr(TrSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type U(USEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type G(GSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type A(ASEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type C(CSEXP);
+    Rcpp::traits::input_parameter< double >::type sigma_b(sigma_bSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Gs(GsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type As(AsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Cs(CsSEXP);
+    Rcpp::traits::input_parameter< double >::type sigma_bs(sigma_bsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Ks(KsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Xs_centers(Xs_centersSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Omega(OmegaSEXP);
+    Rcpp::traits::input_parameter< std::string >::type model(modelSEXP);
+    rcpp_result_gen = Rcpp::wrap(sample_BBsL_cpp(k, X, Tr, U, G, A, C, sigma_b, Gs, As, Cs, sigma_bs, Ks, Xs_centers, Omega, model));
+    return rcpp_result_gen;
+END_RCPP
+}
+// sample_BBsL_parallel
+List sample_BBsL_parallel(arma::mat k, arma::mat X, arma::mat Tr, arma::mat U, arma::mat G, arma::mat A, arma::mat C, double sigma_b, arma::mat Gs, arma::mat As, arma::mat Cs, double sigma_bs, arma::mat Ks, arma::mat Xs_centers, arma::mat Omega, std::string model);
+RcppExport SEXP _occJSDM_sample_BBsL_parallel(SEXP kSEXP, SEXP XSEXP, SEXP TrSEXP, SEXP USEXP, SEXP GSEXP, SEXP ASEXP, SEXP CSEXP, SEXP sigma_bSEXP, SEXP GsSEXP, SEXP AsSEXP, SEXP CsSEXP, SEXP sigma_bsSEXP, SEXP KsSEXP, SEXP Xs_centersSEXP, SEXP OmegaSEXP, SEXP modelSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< arma::mat >::type k(kSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type X(XSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Tr(TrSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type U(USEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type G(GSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type A(ASEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type C(CSEXP);
+    Rcpp::traits::input_parameter< double >::type sigma_b(sigma_bSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Gs(GsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type As(AsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Cs(CsSEXP);
+    Rcpp::traits::input_parameter< double >::type sigma_bs(sigma_bsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Ks(KsSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Xs_centers(Xs_centersSEXP);
+    Rcpp::traits::input_parameter< arma::mat >::type Omega(OmegaSEXP);
+    Rcpp::traits::input_parameter< std::string >::type model(modelSEXP);
+    rcpp_result_gen = Rcpp::wrap(sample_BBsL_parallel(k, X, Tr, U, G, A, C, sigma_b, Gs, As, Cs, sigma_bs, Ks, Xs_centers, Omega, model));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -566,7 +636,10 @@ static const R_CallMethodDef CallEntries[] = {
     {"_occJSDM_KsBproduct", (DL_FUNC) &_occJSDM_KsBproduct, 3},
     {"_occJSDM_XtOmegaX_SoR", (DL_FUNC) &_occJSDM_XtOmegaX_SoR, 5},
     {"_occJSDM_sampleB_SoR", (DL_FUNC) &_occJSDM_sampleB_SoR, 8},
+    {"_occJSDM_sampleB_SoR_TS", (DL_FUNC) &_occJSDM_sampleB_SoR_TS, 8},
     {"_occJSDM_spatEffectMeanCpp", (DL_FUNC) &_occJSDM_spatEffectMeanCpp, 3},
+    {"_occJSDM_sample_BBsL_cpp", (DL_FUNC) &_occJSDM_sample_BBsL_cpp, 16},
+    {"_occJSDM_sample_BBsL_parallel", (DL_FUNC) &_occJSDM_sample_BBsL_parallel, 16},
     {NULL, NULL, 0}
 };
 

src/functions.cpp
@@ -56,8 +56,6 @@ void setOccJSDMSeed(unsigned int seed) {
 #define MATH_LOG_2_PI  -0.45158270528945486472619522989488214357179467855505631739
 #define MATH_LOG_PI_2  0.451582705289454864726195229894882143571794678555056317392
 
-// old code
-
 static double aterm(int n, double x, double t) {
   double f = 0;
   if(x <= t) {
@@ -295,7 +293,7 @@ static double rpg(int n, double z){
 
   double x = 0;
   for(int i = 0; i < n; i++){
-    x += samplepg(z);
+    x += samplepg_fast(z);
   }
 
   return(x);
@@ -429,6 +427,44 @@ static arma::vec sample_Omega_cpp(arma::mat& X, arma::vec& beta, arma::vec& n){
   return(Omega_vec);
 }
 
+struct SampleOmegaWorker : public RcppParallel::Worker {
+
+  // Use raw Armadillo references directly (no RMatrix/RVector)
+  const arma::mat& X;
+  const arma::vec& beta;
+  const arma::vec& n;
+  arma::vec& Omega_vec;
+
+  SampleOmegaWorker(const arma::mat& X, const arma::vec& beta, const arma::vec& n, arma::vec& Omega_vec)
+    : X(X), beta(beta), n(n), Omega_vec(Omega_vec) {}
+
+  void operator()(std::size_t begin, std::size_t end) {
+    // Standard Armadillo property
+    std::size_t p = beta.n_elem;
+
+    for (std::size_t i = begin; i < end; ++i) {
+      // Calculate dot product
+      double b = 0.0;
+      for (std::size_t j = 0; j < p; ++j) {
+        b += X(i, j) * beta[j];
+      }
+
+      // Safe thread_local sampling
+      Omega_vec[i] = rpg(n[i], b);
+    }
+  }
+};
+
+arma::vec sample_Omega_parallel(arma::mat& X, arma::vec& beta, arma::vec& n) {
+  int nsize = n.size();
+  arma::vec Omega_vec(nsize);
+
+  SampleOmegaWorker worker(X, beta, n, Omega_vec);
+  RcppParallel::parallelFor(0, nsize, worker);
+
+  return Omega_vec;
+}
+
 static arma::vec sample_beta_nocov_cpp(arma::vec beta, arma::mat& X, arma::vec b,
                                 arma::mat B, arma::vec n, arma::vec k){
 
@@ -442,7 +478,8 @@ static arma::vec sample_beta_nocov_cpp(arma::vec beta, arma::mat& X, arma::vec b
 static arma::vec sample_beta_nocov_cpp_TS(arma::vec beta, arma::mat& X, arma::vec b,
                                 arma::mat B, arma::vec n, arma::vec k){
 
-  arma::vec Omega = sample_Omega_cpp(X, beta, n);
+  // arma::vec Omega = sample_Omega_cpp(X, beta, n);
+  arma::vec Omega = sample_Omega_parallel(X, beta, n);
 
   beta = sample_beta_cpp_TS(X, B, b, Omega, k);
 
@@ -960,7 +997,7 @@ arma::mat sample_betatheta_cpp_parallel(const arma::mat& w,
 
   // Tell OpenMP to parallelize this loop.
   // All variables declared inside the loop become private to each thread.
-  #pragma omp parallel for
+  // #pragma omp parallel for
   for (int s = 0; s < S; ++s) {
 
     // Get the s-th column of z_all

src/jsdm.cpp
@@ -15,7 +15,9 @@
 
 using namespace Rcpp;
 
-// [[Rcpp::depends(RcppArmadillo)]]
+#include <RcppParallel.h>
+
+// [[Rcpp::depends(RcppArmadillo, RcppParallel)]]
 
 // TRUNC = 0.64 and TRUNC_RECIP = 1/0.64, the truncation point from
 // Windle (2013), were declared here but never used, and drew an
@@ -850,6 +852,47 @@ arma::vec sampleB_SoR(arma::mat X, arma::mat &invB, arma::vec &b,
 }
 
 // [[Rcpp::export]]
+arma::vec sampleB_SoR_TS(arma::mat X, arma::mat &invB, arma::vec &b,
+                      arma::vec &k, arma::vec Omega,
+                      arma::mat &X_s_index,
+                      arma::mat &Ks,
+                      int X_centers) {
+
+  // It is assumed that XtOmegaX_SoR and XtK_SoR are thread-safe
+  // and do not modify shared state or call R's RNG.
+  arma::mat XtOmegaX = XtOmegaX_SoR(X, X_centers, Omega, X_s_index, Ks);
+  arma::mat tXk = XtK_SoR(X, X_s_index, Ks, k, X_centers);
+
+  arma::mat Lambda_B = XtOmegaX + invB;
+  arma::vec mu_B = tXk + invB * b;
+
+  arma::mat L = arma::trans(arma::chol(Lambda_B));
+  arma::vec tmp = arma::solve(arma::trimatl(L), mu_B);
+  arma::vec alpha = arma::solve(arma::trimatu(arma::trans(L)), tmp);
+
+  // ---------------------------------------------------------
+  // THREAD-SAFE RNG
+  // Using thread_local ensures each worker thread gets its own
+  // independent generator, initialized only once per thread.
+  // ---------------------------------------------------------
+  thread_local std::random_device rd;
+  thread_local std::mt19937 gen(rd());
+  std::normal_distribution<double> dist(0.0, 1.0);
+
+  arma::vec z(invB.n_cols);
+  for(size_t i = 0; i < z.n_elem; ++i) {
+    z[i] = dist(gen);
+  }
+  // ---------------------------------------------------------
+
+  arma::vec v = arma::solve(arma::trimatu(arma::trans(L)), z);
+
+  arma::vec result = v + alpha;
+
+  return result;
+}
+
+// [[Rcpp::export]]
 arma::mat spatEffectMeanCpp(arma::cube& Bs_output,
                             arma::mat& Ks,
                             arma::mat& Xs_centers) {
@@ -870,4 +913,299 @@ arma::mat spatEffectMeanCpp(arma::cube& Bs_output,
   }
 
   return spatEffect_mean;
+}
+
+
+inline arma::mat computeBtcoef_cpp(const arma::mat& G,
+                                   const arma::mat& Tr,
+                                   const arma::mat& A,
+                                   const arma::mat& C,
+                                   const arma::mat& Btilde) {
+  return Tr * G + A * C + Btilde;
+}
+
+// [[Rcpp::export]]
+List sample_BBsL_cpp(arma::mat k,
+                 arma::mat X,
+                 arma::mat Tr,
+                 arma::mat U,
+                 arma::mat G,
+                 arma::mat A,
+                 arma::mat C,
+                 double sigma_b,
+                 arma::mat Gs,
+                 arma::mat As,
+                 arma::mat Cs,
+                 double sigma_bs,
+                 arma::mat Ks,
+                 arma::mat Xs_centers,
+                 arma::mat Omega,
+                 std::string model) {
+
+  int p = X.n_cols;
+  int ps = Cs.n_cols;
+  int d = U.n_cols;
+  int S = Omega.n_cols;
+
+  arma::mat zero_Sp(S, p, arma::fill::zeros);
+  arma::mat zero_Sps(S, ps, arma::fill::zeros);
+
+  arma::mat M_B = computeBtcoef_cpp(G, Tr, A, C, zero_Sp).t();
+  arma::mat M_Bs = computeBtcoef_cpp(Gs, Tr, As, Cs, zero_Sps).t();
+
+  arma::vec B0(S, arma::fill::zeros);
+  arma::mat B(p, S, arma::fill::zeros);
+  arma::mat Bs(ps, S, arma::fill::zeros);
+  arma::mat L(d, S, arma::fill::zeros);
+
+  int total_dim = 1 + p + d + ps;
+
+  if (total_dim > 0) {
+
+    arma::vec diag_B(total_dim, arma::fill::ones);
+
+    // SAFEGUARD: Only subvec if p > 0 and ps > 0
+    if (p > 0) {
+      diag_B.subvec(1, p).fill(std::pow(sigma_b, 2));
+    }
+    if (ps > 0) {
+      diag_B.subvec(1 + p + d, p + d + ps).fill(std::pow(sigma_bs, 2));
+    }
+
+    arma::mat invB_current = arma::diagmat(1.0 / diag_B);
+
+    // Pre-construct design matrix XU = [1, X, U]
+    arma::mat XU(X.n_rows, 1 + p + d);
+    XU.col(0).ones();
+
+    // SAFEGUARD: Only assign columns if dimensions exist
+    if (p > 0) {
+      XU.cols(1, p) = X;
+    }
+    if (d > 0) {
+      XU.cols(1 + p, p + d) = U;
+    }
+
+    for (int s = 0; s < S; ++s) {
+
+      arma::vec k_current;
+      if (model == "continuous") {
+        k_current = k.col(s) % Omega.col(s);
+      } else {
+        k_current = k.col(s);
+      }
+
+      arma::vec b_current(total_dim, arma::fill::zeros);
+
+      // SAFEGUARD: Only assign subvec if p > 0
+      if (p > 0) {
+        b_current.subvec(1, p) = M_B.col(s);
+      }
+
+      arma::vec omega_s = Omega.col(s);
+
+      arma::vec BBsL = sampleB_SoR(
+        XU,
+        invB_current,
+        b_current,
+        k_current,
+        omega_s,
+        Xs_centers,
+        Ks,
+        ps
+      );
+
+      // SAFEGUARD: Extract parts conditionally
+      B0(s) = BBsL(0);
+      if (p > 0) {
+        B.col(s) = BBsL.subvec(1, p);
+      }
+      if (d > 0) {
+        L.col(s) = BBsL.subvec(1 + p, p + d);
+      }
+      if (ps > 0) {
+        Bs.col(s) = BBsL.subvec(1 + p + d, p + d + ps);
+      }
+    }
+  }
+
+  arma::mat Bt = B.t() - Tr * G - A * C;
+  arma::mat Bts = Bs.t() - Tr * Gs - As * Cs;
+
+  return List::create(
+    Named("B")   = B,
+    Named("Bt")  = Bt,
+    Named("L")   = L,
+    Named("Bs")  = Bs,
+    Named("Bts") = Bts,
+    Named("B0")  = B0
+  );
+}
+
+// ---------------------------------------------------------
+// 1. Define the RcppParallel Worker
+// ---------------------------------------------------------
+struct BBSL_Worker : public RcppParallel::Worker {
+
+  // Inputs (Read-only)
+  const std::string model;
+  const int p, ps, d, total_dim;
+  const arma::mat& k;
+  const arma::mat& Omega;
+  const arma::mat& M_B;
+  const arma::mat& XU;
+
+  // Shared references passed into sampleB_SoR
+  // (Assuming sampleB_SoR does not modify these, otherwise race conditions will occur)
+  arma::mat& invB_current;
+  arma::mat& Xs_centers;
+  arma::mat& Ks;
+
+  // Outputs (Written to distinct columns, so thread-safe)
+  arma::vec& B0;
+  arma::mat& B;
+  arma::mat& Bs;
+  arma::mat& L;
+
+  // Constructor to initialize the worker with references to the data
+  BBSL_Worker(const std::string model, int p, int ps, int d, int total_dim,
+              const arma::mat& k, const arma::mat& Omega, const arma::mat& M_B, const arma::mat& XU,
+              arma::mat& invB_current, arma::mat& Xs_centers, arma::mat& Ks,
+              arma::vec& B0, arma::mat& B, arma::mat& Bs, arma::mat& L)
+    : model(model), p(p), ps(ps), d(d), total_dim(total_dim),
+      k(k), Omega(Omega), M_B(M_B), XU(XU),
+      invB_current(invB_current), Xs_centers(Xs_centers), Ks(Ks),
+      B0(B0), B(B), Bs(Bs), L(L) {}
+
+  // The parallel execution function
+  void operator()(std::size_t begin, std::size_t end) {
+    for (std::size_t s = begin; s < end; ++s) {
+
+      // Thread-local data
+      arma::vec k_current;
+      if (model == "continuous") {
+        k_current = k.col(s) % Omega.col(s);
+      } else {
+        k_current = k.col(s);
+      }
+
+      arma::vec b_current(total_dim, arma::fill::zeros);
+      if (p > 0) {
+        b_current.subvec(1, p) = M_B.col(s);
+      }
+
+      arma::vec omega_s = Omega.col(s);
+
+      // Call the external C++ function
+      // (Uses ps as the X_centers integer parameter based on your previous R code)
+      arma::vec BBsL = sampleB_SoR(
+        XU,
+        invB_current,
+        b_current,
+        k_current,
+        omega_s,
+        Xs_centers,
+        Ks,
+        ps
+      );
+
+      // Write results to the specific column 's'
+      B0(s) = BBsL(0);
+      if (p > 0) {
+        B.col(s) = BBsL.subvec(1, p);
+      }
+      if (d > 0) {
+        L.col(s) = BBsL.subvec(1 + p, p + d);
+      }
+      if (ps > 0) {
+        Bs.col(s) = BBsL.subvec(1 + p + d, p + d + ps);
+      }
+    }
+  }
+};
+
+
+// ---------------------------------------------------------
+// 2. The Main Exported Function
+// ---------------------------------------------------------
+// [[Rcpp::export]]
+List sample_BBsL_parallel(arma::mat k,
+                 arma::mat X,
+                 arma::mat Tr,
+                 arma::mat U,
+                 arma::mat G,
+                 arma::mat A,
+                 arma::mat C,
+                 double sigma_b,
+                 arma::mat Gs,
+                 arma::mat As,
+                 arma::mat Cs,
+                 double sigma_bs,
+                 arma::mat Ks,
+                 arma::mat Xs_centers,
+                 arma::mat Omega,
+                 std::string model) {
+
+  int p = X.n_cols;
+  int ps = Cs.n_cols;
+  int d = U.n_cols;
+  int S = Omega.n_cols;
+
+  arma::mat zero_Sp(S, p, arma::fill::zeros);
+  arma::mat zero_Sps(S, ps, arma::fill::zeros);
+
+  arma::mat M_B = computeBtcoef_cpp(G, Tr, A, C, zero_Sp).t();
+  arma::mat M_Bs = computeBtcoef_cpp(Gs, Tr, As, Cs, zero_Sps).t();
+
+  arma::vec B0(S, arma::fill::zeros);
+  arma::mat B(p, S, arma::fill::zeros);
+  arma::mat Bs(ps, S, arma::fill::zeros);
+  arma::mat L(d, S, arma::fill::zeros);
+
+  int total_dim = 1 + p + d + ps;
+
+  if (total_dim > 0) {
+
+    arma::vec diag_B(total_dim, arma::fill::ones);
+
+    if (p > 0) {
+      diag_B.subvec(1, p).fill(std::pow(sigma_b, 2));
+    }
+    if (ps > 0) {
+      diag_B.subvec(1 + p + d, p + d + ps).fill(std::pow(sigma_bs, 2));
+    }
+
+    arma::mat invB_current = arma::diagmat(1.0 / diag_B);
+
+    arma::mat XU(X.n_rows, 1 + p + d);
+    XU.col(0).ones();
+
+    if (p > 0) {
+      XU.cols(1, p) = X;
+    }
+    if (d > 0) {
+      XU.cols(1 + p, p + d) = U;
+    }
+
+    // Initialize the RcppParallel Worker
+    BBSL_Worker worker(model, p, ps, d, total_dim,
+                       k, Omega, M_B, XU,
+                       invB_current, Xs_centers, Ks,
+                       B0, B, Bs, L);
+
+    // Call parallelFor over the columns 0 to S
+    RcppParallel::parallelFor(0, S, worker);
+  }
+
+  arma::mat Bt = B.t() - Tr * G - A * C;
+  arma::mat Bts = Bs.t() - Tr * Gs - As * Cs;
+
+  return List::create(
+    Named("B")   = B,
+    Named("Bt")  = Bt,
+    Named("L")   = L,
+    Named("Bs")  = Bs,
+    Named("Bts") = Bts,
+    Named("B0")  = B0
+  );
 }

commit da6ed27d7cd6ebd6e06f7bef7c7eb1c773f06ab5

Run Alex's continuous discriminator for B0; file the RNG race it exposed
Alex's correction on B5: ncov_theta = 0 removes beta_theta's slopes but
necessarily keeps its intercept, and in an occupancy model that intercept
and B0 are confounded at M = 2, so nocollcov's -0.0633 residual could be
that rather than a B0 defect. model = "continuous" has no beta_theta at
all. Added it as a diagnostic arm alongside nocollcov rather than in the
production grid, since run_study.R takes the whole list unless filtered
and this would otherwise turn the 10-cell headline table into 11.

Result at R = 200 (PLAN.md 16.4): B0 bias +0.0066, SE 0.0056, 1.2 SE from
zero. Two arms with no beta_theta, on different likelihoods and different
sampler branches, both at zero -- so binary's clean result was not an
artefact of its other differences, and Alex's reading is supported. B5 is
downstream of the beta_theta slope item on this evidence, but does not
close today: the base/nocollcov/binary figures were measured under the
race below and want re-running race-free before the cross-arm arithmetic
is trusted.

The run exposed a regression in 41abe69, now group B item 8. BBSL_Worker
runs under RcppParallel::parallelFor and calls the non-thread-safe
sampleB_SoR(), whose arma::randn() is routed to R's global RNG by
ARMA_RNG_ALT -- the very call Fixed bugs 28 recorded as safe, which was
true only while it was reached single-threaded. Two fits under one seed
differ by 4.34 on B0 and 0.63 on p; at one thread they are bit-for-bit
identical. test-regression-bugs.R:267/269 catch it, which is what that
test is for. Not merely a reproducibility problem: a race on the MT state
permits duplicate and torn draws, so the chain may not target the
intended posterior. The adjacent sampleB_SoR_TS() would close the race
but seeds from OS entropy, so set.seed() would still not control it;
get_rng() in src/rng.h is the scheme that does both.

The continuous arm was therefore run with RCPP_PARALLEL_NUM_THREADS=1,

tests/testthat/helper-simstudy.R
@@ -147,7 +147,32 @@ simstudy_scenarios <- function() {
     #
     # Read it against B0 bias, not coverage: B0 covers at 0.94-0.96 in every
     # cell including `binary`, so coverage cannot distinguish anything here.
-    mk("nocollcov", ncov_theta = 0L, seed_label = "nocollcov"))
+    mk("nocollcov", ncov_theta = 0L, seed_label = "nocollcov"),
+
+    # --- Continuous, B0 with no beta_theta anywhere (PLAN.md 16) --------
+    #
+    # Alex's correction to what `nocollcov` was meant to test. Setting
+    # ncov_theta = 0 removes the beta_theta *slopes* but necessarily keeps
+    # its intercept row, logit(theta_baseline), and in an occupancy model
+    # that intercept and B0 are both intercepts on the same linear
+    # predictor chain: psi sets how often a site is occupied, theta sets
+    # how often an occupied site yields a positive sample, and with M = 2
+    # the data barely separates them. So `nocollcov`'s residual -0.0633
+    # bias could be that confounding rather than a defect in B0 itself,
+    # which is what he was actually proposing to rule out.
+    #
+    # model = "continuous" is the arm with no beta_theta at all: z is
+    # drawn as Normal(eta, tau) and observed directly, so there is no
+    # detection stage, no latent w, and B0 is estimated straight from the
+    # Gaussian likelihood. `binary` already removes beta_theta too, but it
+    # goes through the Polya-Gamma path; continuous is a different
+    # likelihood and a different branch of the sampler, so agreement
+    # between the two is worth more than either alone.
+    #
+    # Same caveat as `binary`, stated rather than glossed: this changes
+    # many things at once, not one. It is a second independent reading of
+    # "B0 with nothing confounded against it", not a controlled contrast.
+    mk("continuous", model = "continuous"))
 }
 
 # --- Seam 1: how the true parameters are chosen --------------------------